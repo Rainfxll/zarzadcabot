97582f7746a1953bccef6185fa109d59b0dc901a
Rainfxll <48831175+Rainfxll@users.noreply.github.com>
2019-09-23 20:17:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,9 @@ const config = require("./config.json");
 
 client.on("ready", () => {
     console.log(`(SYSTEM) Zarządca Jest gotowy do pracy`);
-    client.user.setActivity(`Użytkownicy ${client.users.size} | v0.1`);
+    client.user.setActivity(`Użytkownicy ${client.users.size} | v0.1`);\
+    
+    client.channels.get("625757418957045764").setName(`📑┃Serwery: ${client.guilds.size}/2`)
 
 })
 
